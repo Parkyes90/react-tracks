b086a1d05ad44e7f4467d17a15c7e89f645302ff
parkyes90 <parkyes90@gmail.com>
2019-04-11 18:25:48 +0900

    add backend

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -7,3 +8,12 @@ class Track(models.Model):
     description = models.TextField(blank=True)
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
+    track = models.ForeignKey(
+        'tracks.Track', related_name='likes', on_delete=models.CASCADE)
