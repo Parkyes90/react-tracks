b5f3314676e201934327aa6a1f1063ca392c2b56
parkyes90 <parkyes90@gmail.com>
2019-04-17 18:27:39 +0900

    add create track dialog

--- react-tracks-client/src/components/Track/CreateTrack.js
+++ react-tracks-client/src/components/Track/CreateTrack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -15,10 +15,87 @@ import ClearIcon from "@material-ui/icons/Clear";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 const CreateTrack = ({ classes }) => {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState("");
+
+  const handleAudioChange = event => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+
   return (
-    <Button variant="fab" className={classes.fab} color="secondary">
-      <AddIcon />
-    </Button>
+    <div>
+      <Button
+        variant="fab"
+        className={classes.fab}
+        color="secondary"
+        onClick={() => setOpen(true)}
+      >
+        {open ? <ClearIcon /> : <AddIcon />}
+      </Button>
+      <Dialog open={open} className={classes.dialog}>
+        <form>
+          <DialogTitle>Create Track</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Add a Title, Description & Audio File
+            </DialogContentText>
+            <FormControl fullWidth>
+              <TextField
+                label="Title"
+                placeholder="Add Title"
+                className={classes.textField}
+                onChange={event => setTitle(event.target.value)}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                multiline
+                rows={4}
+                label="Description"
+                placeholder="Add Description"
+                className={classes.textField}
+                onChange={event => setDescription(event.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <input
+                id="audio"
+                required
+                type="file"
+                accept="audio/mp3,audio/wav"
+                className={classes.input}
+                onChange={handleAudioChange}
+              />
+              <label htmlFor="audio">
+                <Button
+                  variant="outlined"
+                  color={file ? "secondary" : "inherit"}
+                  component="span"
+                  className={classes.button}
+                >
+                  Audio File
+                  <LibraryMusicIcon className={classes.icon} />
+                </Button>
+                {file && file.name}
+              </label>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button className={classes.cancel} onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button type="submit" className={classes.save} disabled={
+              !title.trim() || !description.trim() || !file
+            }>
+              Add Track
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
   );
 };
 
